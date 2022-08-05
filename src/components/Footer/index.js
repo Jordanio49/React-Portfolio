bb52f44575bcdd83bd695cc39f9e20b16aedea93
Jordan Thomas <jthomas135@hotmail.com>
2022-08-05 12:15:57 -0700

    added code for header and footer. still a bit left to do

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <a href='https://github.com/Jordanio49' target='_blank'>
+                {/* add github icon here */}
+                </a>
+            </div>
+            <div>
+                <a href='https://www.linkedin.com/in/jordan-thomas-b405379a/' target='_blank'>
+                {/* add linkedin icon here */}
+                </a>
+            </div>
+            <div>
+                <a href='https://stackoverflow.com/users/18175443/jordanio49' target='_blank'>
+                    {/* add stackoverflow icon here */}
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
