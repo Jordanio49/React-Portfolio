3db3b6c4da4e237598b6ba792ff639e94661372c
Jordan Thomas <jthomas135@hotmail.com>
2022-08-05 12:45:39 -0700

    added code to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
 
 function App() {
+  const [currentTab, setCurrentTab] = useState("about");
+
+  const tabRender = () => {
+    switch (currentTab) {
+      case "about":
+        return <About />
+      case "portfolio":
+        return <Portfolio />
+      case "contact":
+        return <Contact />
+      case "resume":
+        return <Resume />
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+      </div>
+      <div>
+        <main>{tabRender()}</main>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
+
   );
 }
 
