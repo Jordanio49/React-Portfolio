96e7517f537ce2c90231bb0e8214ed26e7a52e67
Jordan Thomas <jthomas135@hotmail.com>
2022-08-05 12:33:19 -0700

    added github, linkedin, stack overflow icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,17 +5,17 @@ function Footer() {
         <footer>
             <div>
                 <a href='https://github.com/Jordanio49' target='_blank'>
-                {/* add github icon here */}
+                <img src='../../assets/images/github.png' alt='Github'></img>
                 </a>
             </div>
             <div>
                 <a href='https://www.linkedin.com/in/jordan-thomas-b405379a/' target='_blank'>
-                {/* add linkedin icon here */}
+                <img src='../../assets/images/linkedIn.jpg' alt='LinkedIn'></img>
                 </a>
             </div>
             <div>
                 <a href='https://stackoverflow.com/users/18175443/jordanio49' target='_blank'>
-                    {/* add stackoverflow icon here */}
+                <img src='../../assets/images/stackoverflow.jpg' alt='Stack Overflow'></img>
                 </a>
             </div>
         </footer>
